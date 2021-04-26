a2963bdb0365895b8e8df6d3f61918e9b0351ea9
ugers <yugers@gmail.com>
2021-04-26 14:48:31 +0300

    fix

--- build_res.bat
+++ build_res.bat
@@ -1,2 +1,2 @@
 @echo off
-ResPack.exe -a BipOS_0.5.5.res.latin ./stock/BipOS_0.5.5.res.latin Music_Control_Mod.elf
+ResPack.exe -a BipOS_0.5.5.res.latin ./stock/BipOS_0.5.5.res.latin Music_Control.elf
